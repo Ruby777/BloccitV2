0583ead1f27a134a2c8de6e8373891d9e5966fa0
Lucy <lucynn11@gmail.com>
2018-05-23 22:05:31 +0400

    rails-seeding-data-assignment-complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,11 @@ require 'random_data'
     )
 end
 
+   Post.find_or_create_by!(
+         title: "the week rushed by",
+         body: "it's weird how I lose track of time sometimes"
+)
+
 posts = Post.all
 
 #Create Comments
@@ -17,6 +22,11 @@ posts = Post.all
         body: RandomData.random_paragraph
     )
 end
+
+   Comment.find_or_create_by!(
+       post: Post.find(101),
+       body: "music makes the world go round"
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
