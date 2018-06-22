7b15e451d2e46d85afaccb5b2bf63b1b27d637be
Lucy <lucynn11@gmail.com>
2018-06-22 17:36:28 +0400

    modification to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  
-  get 'sponsored_posts/show'
-  get 'sponsored_posts/new'
-  get 'sponsored_posts/edit'
+
+  resources :sponsored_posts, only: [:new, :edit, :show]
   
   resources :topics do
     resources :posts, except: [:index]
