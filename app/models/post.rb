8be98889aabbb2b1467f4e55d3f898fc24fcceae
Lucy <lucynn11@gmail.com>
2018-07-03 20:34:32 +0400

    my-solution

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,9 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments, dependent: :destroy
 
-    default_scope { order('created_at DESC')}
+    default_scope { order('created_at DESC') }
+    Post.unscoped { order('title') }
+    Post.unscoped { order('reverse_created_at') }
 
     validates :title, length: { minimum: 5 }, presence: true
     validates :body, length: { minimum: 20 }, presence: true
