c1538dc49176f87ab69241f2f16eac57ac51183d
Lucy <lucynn11@gmail.com>
2018-07-04 20:26:58 +0400

    rails-authorization-complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_01_042154) do
+ActiveRecord::Schema.define(version: 2018_07_03_172842) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2018_07_01_042154) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
