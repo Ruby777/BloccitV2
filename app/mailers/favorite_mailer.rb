3b5df8c73d5fd069428c841317db01816b9482a7
Lucy <lucynn11@gmail.com>
2018-07-22 19:22:50 +0400

    rails-favorites-assignment-complete

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,16 +1,25 @@
 class FavoriteMailer < ApplicationMailer
-   default from: "lucynn11@gmail.com"
+  default from: "lucynn11@gmail.com"
 
-   def new_comment(user, post, comment)
- 
-        headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-        headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-        headers["References"] = "<post/#{post.id}@your-app-name.example>"
-    
-        @user = user
-        @post = post
-        @comment = comment
-    
-        mail(to: user.email, subject: "New comment on #{post.title}")
-      end
+  def new_comment(user, post, comment)
+    headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @user = user
+    @post = post
+    @comment = comment
+
+    mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "You're following, #{post.title}")
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :votes, dependent: :destroy
     has_many :favorites, dependent: :destroy
+    after_create :create_favorite
 
     default_scope { order('rank DESC') }
-
 
     validates :title, length: { minimum: 5 }, presence: true
     validates :body, length: { minimum: 20 }, presence: true
@@ -16,7 +16,6 @@ class Post < ApplicationRecord
     def up_votes
         votes.where(value: 1).count
     end
-
 
     def down_votes
         votes.where(value: -1).count
@@ -30,5 +29,10 @@ class Post < ApplicationRecord
         age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
         new_rank = points + age_in_days
         update_attribute(:rank, new_rank)
+    end
+
+    def create_favorite
+        Favorite.create(post: self, user: self.user)
+        FavoriteMailer.new_post(self).deliver_now
     end
 end
