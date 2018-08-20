f9f8727ed9eaac44d670ca6add6fe7920ce60461
Lucy <lucynn11@gmail.com>
2018-08-20 13:21:13 +0400

    WIP

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,7 @@ module CommentsHelper
     def user_is_authorized_for_comment?(comment)
         current_user && (current_user == comment.user || current_user.admin?)
     end
+    
+    def user_has_existing_comments?
+    end
 end
